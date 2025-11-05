30d9aa7c193ec5732dc6a3d1df0cc4549e3a8a9e
Marco Pistore <marco.pistore@gmail.com>
2025-11-05 18:25:12 +0100

    fix: string format

--- validation/dati_001_tourism.py
+++ validation/dati_001_tourism.py
@@ -14,5 +14,5 @@ print(f"Datasets in project {PROJECT}:")
 for di in list:
     name = di.metadata.name
     updated = datetime.strptime(di.metadata.updated, '%Y-%m-%dT%H:%M:%S.%fZ')
-    print(f"  {name} (last updated: {updated:%d/%m/%Y %H:%M.:%S})")
+    print(f"  {name} (last updated: {updated:%d/%m/%Y %H:%M:%S})")
 print(f"{len(list)} datasets.")
